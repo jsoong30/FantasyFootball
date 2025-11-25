7eb06e83a0b6280999b8f7e7532eeaafa493ff24
Jackson J Soong <jacksonjsoong@gmail.com>
2025-11-25 12:07:10 -0500

    Loadiing teams data

--- src/main/java/com/firstember/fantasyfootball/domain/Team.java
+++ src/main/java/com/firstember/fantasyfootball/domain/Team.java
@@ -13,22 +13,17 @@ public class Team {
     private String code;   // e.g., KC, BUF, PHI
 
     @Column(nullable = false)
-    private String city;   // e.g., Kansas City
-
-    @Column(nullable = false)
     private String name;   // e.g., Chiefs
 
     public Team() {}
     public Team(String code, String city, String name) {
-        this.code = code; this.city = city; this.name = name;
+        this.code = code; this.name = name;
     }
 
     // getters & setters
     public Long getId() { return id; }
     public String getCode() { return code; }
     public void setCode(String code) { this.code = code; }
-    public String getCity() { return city; }
-    public void setCity(String city) { this.city = city; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
