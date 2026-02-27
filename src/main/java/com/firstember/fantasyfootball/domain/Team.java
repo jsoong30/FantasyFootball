ff7dc7db3f0527cc63dc3500c0519f49c54c22a2
Jackson J Soong <jacksonjsoong@gmail.com>
2026-02-27 12:39:35 -0500

    edit

--- src/main/java/com/firstember/fantasyfootball/domain/Team.java
+++ src/main/java/com/firstember/fantasyfootball/domain/Team.java
@@ -16,14 +16,18 @@ public class Team {
     private String name;   // e.g., Chiefs
 
     public Team() {}
-    public Team(String code, String city, String name) {
-        this.code = code; this.name = name;
+
+    public Team(String code, String name) {
+        this.code = code;
+        this.name = name;
     }
 
     // getters & setters
     public Long getId() { return id; }
+
     public String getCode() { return code; }
     public void setCode(String code) { this.code = code; }
+
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
